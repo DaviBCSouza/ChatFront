9dcc26f21dd25b02678338707198905203c12d38
DaviBCSouza <contato@davibrenno.com>
2023-10-16 20:32:36 -0400

    Creating the Frontend

--- src/main/java/ifam/edu/dra/chatfront/controller/FrontContatoController.java
+++ src/main/java/ifam/edu/dra/chatfront/controller/FrontContatoController.java
@@ -1,5 +1,22 @@
 package ifam.edu.dra.chatfront.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ifam.edu.dra.chatfront.service.FrontContatoService;
+
+@Controller
 public class FrontContatoController {
 
+	@Autowired
+	FrontContatoService frontContatoService;
+
+	@GetMapping("/showContatos")
+	public String showContatos(Model model) {
+
+		model.addAttribute("contatos", frontContatoService.getContatos());
+		return "listContato";
+	}
 }
--- src/main/java/ifam/edu/dra/chatfront/service/FrontContatoService.java
+++ src/main/java/ifam/edu/dra/chatfront/service/FrontContatoService.java
@@ -6,14 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import ifam.edu.dra.chat.model.Contato;
+import ifam.edu.dra.chatfront.model.Contato;
 
 import java.util.List;
 
 @Service
 public class FrontContatoService {
 
-	private final String backendUrl = "http://localhost:8080";
+	private final String backendUrl = "http://localhost:8080/contato";
 	private final RestTemplate restTemplate;
 
 	public FrontContatoService(RestTemplate restTemplate) {
@@ -21,7 +21,7 @@ public class FrontContatoService {
 	}
 
 	public List<Contato> getContatos() {
-		String url = backendUrl + "/contatos";
+		String url = backendUrl;
 		ResponseEntity<List<Contato>> response = restTemplate.exchange(url, HttpMethod.GET, null,
 				new ParameterizedTypeReference<List<Contato>>() {
 				});
@@ -29,23 +29,23 @@ public class FrontContatoService {
 	}
 
 	public Contato getContato(Long id) {
-		String url = backendUrl + "/contato/" + id;
+		String url = backendUrl + id;
 		return restTemplate.getForObject(url, Contato.class);
 	}
 
 	public Contato setContato(Contato contato) {
-		String url = backendUrl + "/contato";
+		String url = backendUrl;
 		return restTemplate.postForObject(url, contato, Contato.class);
 	}
 
 	public Contato updateContato(Long id, Contato contato) {
-		String url = backendUrl + "/contato/" + id;
+		String url = backendUrl + id;
 		restTemplate.put(url, contato);
 		return contato;
 	}
 
 	public void deleteContato(Long id) {
-		String url = backendUrl + "/contato/" + id;
+		String url = backendUrl + id;
 		restTemplate.delete(url);
 	}
 }
